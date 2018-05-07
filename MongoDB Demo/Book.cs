f8931e86356dfbccdb654de159ba98d734a2648a
Young <wyoung@proforma.com>
2018-05-06 20:11:05 -0400

    console app done

---
+++ MongoDB Demo/Book.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDB_Demo
+{
+    public class Book
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int YearPublished { get; set; }
+    }
+}
--- MongoDB Demo/Program.cs
+++ MongoDB Demo/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,24 @@ namespace MongoDB_Demo
     {
         static void Main(string[] args)
         {
+            const string connectionString = "mongodb://localhost:27017";
+
+            var client = new MongoClient(connectionString);
+
+            var database = client.GetDatabase("books");
+
+            var books = database.GetCollection<Book>("books");
+
+            var book = new Book();
+
+            book.Title = "Harry Potter 2";
+            book.YearPublished = 2005;
+            books.InsertOne(book);
+
+            var firstBook = books.Find(b => b.Title == "Harry Potter 2").First();
+
+            Console.WriteLine($"book title: {firstBook.Title}, year published: {firstBook.YearPublished}");
+            Console.Read();
         }
     }
 }
